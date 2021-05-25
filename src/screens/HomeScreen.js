df3cbecf534f128914fbb10ae34ef9ae30f7b631
Harsh Verma <harshverma0362@gmail.com>
2021-05-25 23:37:05 +0530

    added reset btn and functionlity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -41,6 +41,12 @@ const HomeScreen = () => {
     shortLink(originalUrl);
   };
 
+  const resetHandler = () => {
+    setOriginalUrl("");
+    setShortenLink("");
+    setIsShorten(false);
+  };
+
   return (
     <div className="container mx-auto px-3 md:px-12 pt-7 md:pt-10">
       <div className="grid grid-cols-1 md:grid-cols-2">
@@ -73,12 +79,22 @@ const HomeScreen = () => {
               value={originalUrl}
               onChange={(e) => setOriginalUrl(e.target.value)}
             />
-            <button
-              type="submit"
-              className="px-6 py-4 bg-purple-700 text-white text-xl font-semibold transition duration-200 hover:bg-purple-800 focus:outline-none rounded-md mt-5 max-w-max"
-            >
-              Shorten
-            </button>
+            <div className="flex flex-row">
+              <button
+                type="submit"
+                className="px-6 py-4 bg-purple-700 text-white text-xl font-semibold transition duration-200 hover:bg-purple-800 focus:outline-none rounded-md mt-5 max-w-max"
+              >
+                Shorten
+              </button>
+              {isShorten && (
+                <button
+                  onClick={resetHandler}
+                  className="px-6 py-4 ml-4 bg-red-600 text-white text-xl font-semibold transition duration-200 hover:bg-red-800 focus:outline-none rounded-md mt-5 max-w-max"
+                >
+                  Reset
+                </button>
+              )}
+            </div>
           </form>
           {isShorten && (
             <div className="px-7 py-5 bg-gray-100 mt-6 rounded  flex flex-col">
